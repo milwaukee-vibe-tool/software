ba737d2a0dd90b6c66c7cece391c53f7d866a62a
rpbritton <git@rbritton.dev>
2023-02-15 15:18:07 -0500

    Fill out some control functions

--- src/stores/control.ts
+++ src/stores/control.ts
@@ -1,49 +1,114 @@
 import { defineStore } from "pinia";
 import { useConnectionStore } from "./connection";
-import { DeleteLog } from "../protobuf/build/typescript/protos/requests/deleteLog";
-import { FormatSd } from "../protobuf/build/typescript/protos/requests/formatSd";
-import { GetLog } from "../protobuf/build/typescript/protos/requests/getLog";
-import { ListLogs } from "../protobuf/build/typescript/protos/requests/listLogs";
-import { NewLog } from "../protobuf/build/typescript/protos/requests/newLog";
-import { LogList } from "../protobuf/build/typescript/protos/responses/logList";
-import { LogInfo } from "../protobuf/build/typescript/protos/responses/logInfo";
-import { ref } from "vue";
+import { reactive } from "vue";
+import { ErrorCode } from "../protobuf/build/typescript/protos/responses/error";
 
-export interface LogListEntry {
-  name: string;
+export interface LogListStore {
+  loading: boolean;
+  list: string[] | undefined;
 }
 
-export interface LogInfoEntry {
-  logId: string;
+export interface LogInfoStore {
+  loading: boolean;
   sampleRate: number;
   points: [number, number][];
 }
 
+const ErrorResponseType = new Error("Bad response type received");
+
 export const useControlStore = defineStore("control", () => {
   const connectionStore = useConnectionStore();
 
-  const logList = ref<LogListEntry[]>([]);
-  const logInfo = ref(new Map<string, LogInfoEntry>());
+  const THROTTLE_LOGS = 20;
+  const THROTTLE_POINTS = 500;
 
-  async function deleteLog(deleteLog: DeleteLog) {
-    return connectionStore.send({ deleteLog });
-    // todo: remove log
+  // States:
+  // Unloaded: loading=false, list=undefined
+  // Loading: loading=true
+  // Loaded: loading=false, list=string[]
+  const logList = reactive<LogListStore>({ loading: false, list: undefined });
+
+  // States:
+  // Unloaded: map.has(id)=false
+  // Loading: map.has(id)=true, map.get(id)=undefined
+  // Loaded: map.has(id)=true, map.get(id)=LogInfo
+  // Loading points: map.get(id).loading=true
+  const logInfo = reactive<Map<string, LogInfoStore | undefined>>(new Map());
+
+  async function listLogs() {
+    logList.loading = true;
+    let list: string[] = [];
+    let chunkLength = 0;
+    do {
+      let payload = await connectionStore.send({
+        listLogs: { offset: list.length, count: THROTTLE_LOGS },
+      });
+      let chunk = payload.logList;
+      if (chunk == undefined) throw ErrorResponseType;
+      list.concat(chunk.logs.map((entry) => entry.logId));
+      chunkLength = chunk.count;
+    } while (chunkLength < THROTTLE_LOGS);
+    logList.list = list;
+    logList.loading = false;
   }
-  async function formatSd(formatSd: FormatSd) {
-    return connectionStore.send({ formatSd });
-    // todo: remove all files
+  async function newLog(logId: string) {
+    logList.loading = true;
+    await connectionStore.send({ newLog: { logId: logId, sampleRate: 0 } });
+    await listLogs();
   }
-  async function getLog(getLog: GetLog) {
-    return (await connectionStore.send({ getLog })).logInfo;
-    // todo: add to logInfo
+  async function deleteLog(logId: string) {
+    logList.loading = true;
+    await connectionStore.send({ deleteLog: { logId: logId } });
+    await listLogs();
   }
-  async function listLogs(listLogs: ListLogs) {
-    return (await connectionStore.send({ listLogs })).logList;
-    // todo: add to log list
+  async function formatSd() {
+    logList.loading = true;
+    await connectionStore.send({ formatSd: {} });
+    await listLogs();
   }
-  async function newLog(newLog: NewLog) {
-    return connectionStore.send({ newLog });
-    // todo: add to log list
+
+  async function getLog(logId: string) {
+    let info = logInfo.get(logId);
+
+    // check if log has ever been retrieved
+    if (info == undefined) {
+      logInfo.set(logId, undefined);
+      let payload = await connectionStore.send({
+        getLog: { logId: logId, offset: 0, count: THROTTLE_POINTS },
+      });
+      let chunk = payload.logInfo;
+      if (chunk == undefined) {
+        logInfo.delete(logId);
+        if (
+          payload.error != undefined &&
+          payload.error.code == ErrorCode.NOT_FOUND
+        ) {
+          return;
+        } else {
+          throw ErrorResponseType;
+        }
+      }
+      info = {
+        loading: true,
+        sampleRate: chunk.sampleRate,
+        points: chunk.points.map((point) => [point.stamp, point.value]),
+      };
+    }
+
+    // check for more points
+    info.loading = true;
+    let chunkLength = 0;
+    do {
+      let payload = await connectionStore.send({
+        getLog: { logId: logId, offset: 0, count: THROTTLE_POINTS },
+      });
+      let chunk = payload.logList;
+      if (chunk == undefined) throw ErrorResponseType;
+      list.concat(chunk.logs.map((entry) => entry.logId));
+      chunkLength = chunk.count;
+    } while (chunkLength < THROTTLE_LOGS);
+    logList.list = list;
+    info.loading = false;
   }
 
   return { logList, logInfo, deleteLog, formatSd, getLog, listLogs, newLog };
