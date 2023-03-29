36c8eb792e120cb58857c67d912ec27f9410889c
rpbritton <git@rbritton.dev>
2023-03-29 13:06:44 -0400

    Make the start view neater

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,15 +1,16 @@
 <template>
-  <main>
-    <Welcome />
+  <main
+    class="q-pa-md column flex-center full-height"
+    style="min-height: inherit"
+  >
+    <h5 class="text-grey-6" style="text-align: center">
+      Welcome to the Milwaukee Vibe Tool software interface.
+    </h5>
+    <p class="text-grey-6" style="text-align: center">
+      Use the side bar to view offline logs, or connect using Bluetooth using
+      the button in the upper left.
+    </p>
   </main>
 </template>
 
-<script lang="ts">
-import Welcome from "../components/Welcome.vue";
-
-export default {
-  components: {
-    Welcome,
-  },
-};
-</script>
+<script setup lang="ts"></script>
